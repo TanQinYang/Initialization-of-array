9f79c28e49f53875f526502c6e92ebcc0c9b5639
QinYang Tan <108197858+TanQinYang@users.noreply.github.com>
2023-10-30 22:21:33 +0100

    Add files via upload

---
+++ Initialization of array/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*   int arr[4]={1,2,3,4};
+   printf("%d",arr[3]);
+    return 0;
+
+}
+*/
+/*int i;
+ int a[10]={1,2};
+ for(i=0;i<=9;i++)
+  printf("%d\n",a[i]);
+    return 0;
+}
+*/
+ int i;
+ int a[]={1,2,3,4,5,6,7,8,9,10};
+  printf("%d\n",a[11]);
+    return 0;
+}
